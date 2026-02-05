0cfcbaf37b5748f25aed2f2468315223532fed30
YijiaWW <114932018+YijiaWW@users.noreply.github.com>
2026-02-05 18:05:06 -0500

    initial setup
    1

--- CarRentalPlatform/obj/Debug/net8.0/CarRentalPlatform.AssemblyInfo.cs
+++ CarRentalPlatform/obj/Debug/net8.0/CarRentalPlatform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRentalPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afa9df7ceb9b43b3596618d574bc38649f54ae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRentalPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRentalPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
